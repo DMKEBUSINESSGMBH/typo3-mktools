172ccee758197cf287cdb75f8b271cd2c73d3ba3
Christian Riesche <christian.riesche@das-medienkombinat.de>
2012-09-24 10:04:05 +0200

    #2936: Tests für Robots Meta Tag

---
+++ tests/util/class.tx_mktools_tests_util_SeoRobotsMetaTag_testcase.php
@@ -0,0 +1,105 @@
+<?php
+/*  **********************************************************************  **
+ *  Copyright notice
+ *
+ *  (c) 2012 das MedienKombinat GmbH <kontakt@das-medienkombinat.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *  ***********************************************************************  */
+require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
+tx_rnbase::load('tx_mklib_tests_Util');
+tx_rnbase::load('tx_mklib_util_DB');
+tx_rnbase::load('tx_rnbase_util_Misc');
+tx_rnbase::load('tx_mktools_util_SeoRobotsMetaTag');
+/**
+ *
+ * @package tx_mktools
+ * @author Michael Wagner <michael.wagner@das-medienkombinat.de>
+ */
+class tx_mktools_tests_util_SeoRobotsMetaTag_testcase  extends tx_phpunit_database_testcase {
+	protected $workspaceIdAtStart;
+	
+	public function __construct ($name=null) {
+		parent::__construct ($name);
+		$GLOBALS['TYPO3_DB']->debugOutput = TRUE;
+	
+		$this->workspaceIdAtStart = $GLOBALS['BE_USER']->workspace;
+		$GLOBALS['BE_USER']->setWorkspace(0);
+	}
+	
+	public function setUp() {
+		$this->createDatabase();
+		// assuming that test-database can be created otherwise PHPUnit will skip the test
+		$db = $this->useTestDatabase();
+		$this->importStdDB();
+		$this->importExtensions(array('cms', 'mktools', 'templavoila', 'realurl'));
+		$this->importDataSet( t3lib_extMgm::extPath('mktools').'tests/fixtures/xml/pages.xml');
+		tx_rnbase_util_Misc::prepareTSFE();
+		tx_mklib_tests_Util::disableDevlog();
+	}
+	
+	public function tearDown () {
+		$this->cleanDatabase();
+		$this->dropDatabase();
+		$GLOBALS['TYPO3_DB']->sql_select_db(TYPO3_db);
+		$GLOBALS['BE_USER']->setWorkspace($this->workspaceIdAtStart);
+	}
+	
+
+	
+	public function testGetDefaultValueWhenNoValueSetAndNoInheritedValueExists() {
+		$GLOBALS['TSFE']->id = 1;
+		$util = new tx_mktools_util_SeoRobotsMetaTag;
+		$value = $util->getSeoRobotsMetaTagValue('', array('default' => 'test'));
+		
+		$this->assertEquals('test', $value ,'Falscher Wert zurückgeliefert');
+	}
+	
+	public function testGetCorrectValue() {
+		$GLOBALS['TSFE']->id = 2;
+		$util = new tx_mktools_util_SeoRobotsMetaTag;
+		$value = $util->getSeoRobotsMetaTagValue('', array('default' => 'test'));
+		$this->assertEquals('NOINDEX,FOLLOW', $value ,'Falscher Wert zurückgeliefert');
+		
+		$GLOBALS['TSFE']->id = 4;
+		$util = new tx_mktools_util_SeoRobotsMetaTag;
+		$value = $util->getSeoRobotsMetaTagValue('', array('default' => 'test'));
+		
+		$this->assertEquals('INDEX,NOFOLLOW', $value ,'Falscher Wert zurückgeliefert');
+	}
+	
+	public function testGetCorrectInheritedValue() {
+		$GLOBALS['TSFE']->id = 3;
+		$util = new tx_mktools_util_SeoRobotsMetaTag;
+		$value = $util->getSeoRobotsMetaTagValue('', array('default' => 'test'));
+		$this->assertEquals('NOINDEX,FOLLOW', $value ,'Falscher Wert zurückgeliefert');
+		
+		$GLOBALS['TSFE']->id = 5;
+		$util = new tx_mktools_util_SeoRobotsMetaTag;
+		$value = $util->getSeoRobotsMetaTagValue('', array('default' => 'test'));
+		$this->assertEquals('INDEX,NOFOLLOW', $value ,'Falscher Wert zurückgeliefert');
+	}
+	
+	
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']
+		['ext/mktools/tests/util/class.tx_mktools_tests_util_SeoRobotsMetaTag_testcase.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']
+		['ext/mktools/tests/util/class.tx_mktools_tests_util_SeoRobotsMetaTag_testcase.php']);
+}
