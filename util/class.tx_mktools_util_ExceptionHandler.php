1a24b9d172b886e1850bd2030fd2ee5b649596b2
hbochmann <hannes.bochmann@das-medienkombinat.de>
2013-08-30 15:30:34 +0200

    bugfix

--- util/class.tx_mktools_util_ExceptionHandler.php
+++ util/class.tx_mktools_util_ExceptionHandler.php
@@ -195,7 +195,8 @@ class tx_mktools_util_ExceptionHandler extends t3lib_error_ProductionExceptionHa
 	 * @return void
 	 */
 	protected function sendStatusHeaders(Exception $exception) {
-		if (method_exists($this, 'sendStatusHeaders')) {
+		tx_rnbase::load('tx_rnbase_util_TYPO3');
+		if (tx_rnbase_util_TYPO3::isTYPO46OrHigher()) {
 			parent::sendStatusHeaders($exception);
 		} else {
 			if (method_exists($exception, 'getStatusHeaders')) {
