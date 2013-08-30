d2fb4b888874296c28614f672bb9273ca5a893b4
hbochmann <hannes.bochmann@das-medienkombinat.de>
2013-08-30 14:59:04 +0200

    fehlendes autoload

--- ext_autoload.php
+++ ext_autoload.php
@@ -6,5 +6,6 @@
 return array(
 	'tx_mktools_util_errorhandler'							=> t3lib_extMgm::extPath('mktools', 'util/class.tx_mktools_util_ErrorHandler.php'),
 	'tx_mktools_util_exceptionhandler'						=> t3lib_extMgm::extPath('mktools', 'util/class.tx_mktools_util_ExceptionHandler.php'),
+	'tx_mktools_util_errorexception'						=> t3lib_extMgm::extPath('mktools', 'util/class.tx_mktools_util_ErrorException.php'),
 	'tx_mktools_scheduler_generaterealurlconfigurationfile'	=> t3lib_extMgm::extPath('mktools', 'scheduler/class.tx_mktools_scheduler_GenerateRealUrlConfigurationFile.php'),
 );
