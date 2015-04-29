e1e92835c103cfc23efd557cf9b96d92b97b6c25
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2015-04-29 11:52:26 +0200

    benötige rn_base version anpassen

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'constraints' => array(
 		'depends' => array(
- 			'rn_base' => '',
+ 			'rn_base' => '0.14.18-0.0.0',
 			'mklib' => '',
 			'typo3' => '4.3.0-6.2.99',
 		),
