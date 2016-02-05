5471bfbdb8a76e7468557e6d68c3f8b16cb297d9
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2016-02-05 16:27:46 +0100

    AC#235-3372 Kompatibilität gesetzt

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
  			'rn_base' => '0.16.2-0.0.0',
 			'mklib' => '',
-			'typo3' => '4.3.0-6.2.99',
+			'typo3' => '4.3.0-7.6.99',
 		),
 		'conflicts' => array(
 		),
