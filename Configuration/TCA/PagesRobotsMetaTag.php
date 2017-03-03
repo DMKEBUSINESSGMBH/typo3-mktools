39f137dd66f4ebafba2e37fec3e5eadae574b2fc
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2017-03-03 14:58:33 +0100

    bugfix TCA nur einbinden wenn aktiviert

--- Configuration/TCA/PagesRobotsMetaTag.php
+++ Configuration/TCA/PagesRobotsMetaTag.php
@@ -1,6 +1,12 @@
 <?php
 if (!defined ('TYPO3_MODE')) { die ('Access denied.'); }
 
+
+tx_rnbase::load('tx_mktools_util_miscTools');
+if (!tx_mktools_util_miscTools::isSeoRobotsMetaTagActive()) {
+	return;
+}
+
 tx_rnbase::load('tx_rnbase_util_TCA');
 tx_rnbase::load('tx_mktools_util_SeoRobotsMetaTag');
 
