e53c9e3396e0b9b1a022ce02567b0e5723b9ab02
Michael Wagner <michael.wagner@dmk-ebusiness.de>
2017-04-21 07:59:19 +0200

    Cleanup whitespaces in tca testcase

--- tests/class.tx_mktools_tests_TCA_testcase.php
+++ tests/class.tx_mktools_tests_TCA_testcase.php
@@ -26,10 +26,6 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  */
 
-/**
- * benötigte Klassen einbinden
- */
-
 tx_rnbase::load('tx_mklib_tests_Util');
 tx_rnbase::load('tx_mktools_util_miscTools');
 
@@ -37,7 +33,7 @@ tx_rnbase::load('tx_mktools_util_miscTools');
  * @author Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
  */
 class tx_mktools_tests_TCA_testcase extends tx_phpunit_testcase {
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see PHPUnit_Framework_TestCase::setUp()
@@ -45,7 +41,7 @@ class tx_mktools_tests_TCA_testcase extends tx_phpunit_testcase {
 	protected function setUp(){
 		tx_mklib_tests_Util::storeExtConf('mktools');
 	}
-	
+
 	/**
 	 * (non-PHPdoc)
 	 * @see PHPUnit_Framework_TestCase::tearDown()
@@ -53,7 +49,7 @@ class tx_mktools_tests_TCA_testcase extends tx_phpunit_testcase {
 	protected function tearDown(){
 		tx_mklib_tests_Util::restoreExtConf('mktools');
 	}
-	
+
 	/**
 	 * Setzt die TCA zurück und lädt die ext_tables.php erneut
 	 */
@@ -63,53 +59,52 @@ class tx_mktools_tests_TCA_testcase extends tx_phpunit_testcase {
 		$_EXTKEY = 'mktools';
 		include(tx_rnbase_util_Extensions::extPath('mktools', 'ext_tables.php'));
 	}
-	
+
 	/**
 	 * @group unit
 	 */
 	public function testTcaForFixedPostVarTypesIsNotIncludedIfNotSet(){
 		global $TCA;
-		
+
 		tx_mklib_tests_Util::setExtConfVar('tableFixedPostVarTypes', 0, 'mktools');
 		self::loadExtTables();
-		
+
 		$tableFixedPostVarTypes = tx_mktools_util_miscTools::loadFixedPostVarTypesTable();
-		
+
 		$this->assertEquals(
-			0, intval($tableFixedPostVarTypes), 
+			0, intval($tableFixedPostVarTypes),
 			'Die Extension Konfiguration tableFixedPostVarTypes ist falsch gesetzt.'
 		);
 		$this->assertFalse(
-			array_key_exists('tx_mktools_fixedpostvartypes',$TCA), 
+			array_key_exists('tx_mktools_fixedpostvartypes',$TCA),
 			'Die TCA für die tx_mktools_fixedpostvartypes Tabelle wurde geladen.'
 		);
 	}
-	
+
 	/**
 	 * @group unit
 	 */
 	public function testTcaForFixedPostVarTypesIsIncludedIfSet(){
 		global $TCA;
-		
+
 		tx_mklib_tests_Util::setExtConfVar('tableFixedPostVarTypes', 1, 'mktools');
 		self::loadExtTables();
-		
+
 		$tableFixedPostVarTypes = tx_mktools_util_miscTools::loadFixedPostVarTypesTable();
-		
+
 		$this->assertEquals(
-			1, intval($tableFixedPostVarTypes), 
+			1, intval($tableFixedPostVarTypes),
 			'Die Extension Konfiguration tableWordlist ist falsch gesetzt'
 		);
 		$this->assertTrue(
-			array_key_exists('tx_mktools_fixedpostvartypes',$TCA), 
+			array_key_exists('tx_mktools_fixedpostvartypes',$TCA),
 			'Die TCA für die tx_mktools_fixedpostvartypes Tabelle wurde nicht geladen.'
 		);
 		$this->assertTrue(
-			array_key_exists('ctrl',$TCA['tx_mktools_fixedpostvartypes']), 
+			array_key_exists('ctrl',$TCA['tx_mktools_fixedpostvartypes']),
 			'Die TCA für die tx_mktools_fixedpostvartypes Tabelle wurde nicht richtig geladen.'
 		);
 	}
-	
 }
 
 if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mklib/tests/util/class.tx_mklib_tests_TCA_testcase.php']) {
