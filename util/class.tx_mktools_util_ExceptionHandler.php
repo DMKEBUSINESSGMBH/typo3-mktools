774c11f33f2d585b9c631f0948dabd97ee914aa6
Norman Paschke <norman.paschke@dmk-ebusiness.de>
2018-10-04 13:20:08 +0200

    AC#614-271 remove exceptionPage cause its empty

--- util/class.tx_mktools_util_ExceptionHandler.php
+++ util/class.tx_mktools_util_ExceptionHandler.php
@@ -189,7 +189,7 @@ class tx_mktools_util_ExceptionHandlerBase extends Tx_Rnbase_Error_ProductionExc
             $exceptionPage = $configurations->get('errorhandling.exceptionPage');
         } else {
             tx_rnbase::load('tx_rnbase_util_Logger');
-            tx_rnbase_util_Logger::warn('unbekannter error page type "' . $exceptionPageType . '" (möglich: FILE, TYPOSCRIPT), Exception Page: ' . $exceptionPage, 'mktools');
+            tx_rnbase_util_Logger::warn('unbekannter error page type "' . $exceptionPageType . '" (möglich: FILE, TYPOSCRIPT)', 'mktools');
         }
 
         return $exceptionPage;
