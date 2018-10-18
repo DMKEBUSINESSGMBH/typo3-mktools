a33cc4162c31e845b7cca8643e11af6bdbe7a1ba
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2018-10-18 10:28:54 +0200

    cleanup fixed package name

--- tests/Classes/ContentObject/UserInternalContentObjectTest.php
+++ tests/Classes/ContentObject/UserInternalContentObjectTest.php
@@ -28,7 +28,7 @@ namespace DMK\Mktools\ContentObject;
  * DMK\Mktools\ContentObject$UserInternalContentObjectTest
  *
  * @package         TYPO3
- * @subpackage      mkwsw
+ * @subpackage      mktools
  * @author          Hannes Bochmann
  * @license         http://www.gnu.org/licenses/lgpl.html
  *                  GNU Lesser General Public License, version 3 or later
