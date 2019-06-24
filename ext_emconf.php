a8d60dae2fc250eef45cccb63b30f039fd8494cb
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2019-06-24 12:38:27 +0200

    AC#663-3612 set requirement to TYPO3 8.7-9.5
    #7

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'rn_base' => '1.4.0-',
-            'typo3' => '4.5.0-8.7.99',
+            'typo3' => '8.7.99-9.5.99',
             'mklib' => '3.0.0-'
         ),
         'conflicts' => array(),
