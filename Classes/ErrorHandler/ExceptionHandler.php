551d4db8df010256579dd5204e403c077991ff8d
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2024-05-23 16:38:55 +0200

    Cleanup fix cs

--- Classes/ErrorHandler/ExceptionHandler.php
+++ Classes/ErrorHandler/ExceptionHandler.php
@@ -161,10 +161,10 @@ class ExceptionHandler extends ProductionExceptionHandler
 
         if ($this->shouldExceptionBeDebugged()) {
             Debug::debug([
-                    'Exception! Mehr infos im devlog.',
+                'Exception! Mehr infos im devlog.',
             ], __METHOD__.' Line: '.__LINE__);
             Debug::debug([
-                    $exception,
+                $exception,
             ], __METHOD__.' Line: '.__LINE__);
         }
 
