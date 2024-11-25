4271286b68882cef4edd453db22438ca8c800881
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2024-11-25 09:49:45 +0100

    Cleanup fix cs and qs

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -17,5 +17,13 @@ return $config
         'phpdoc_align' => false,
         'no_superfluous_phpdoc_tags' => false,
         'fully_qualified_strict_types' => false,
+        'trailing_comma_in_multiline' => [
+            'after_heredoc' => true,
+            'elements' => [
+                'array_destructuring',
+                'arrays',
+                'match',
+            ],
+        ],
     ])
     ->setLineEnding("\n");
